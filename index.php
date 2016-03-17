031285d3ee8f4e4ba6fe848c5e00119c23a60b6c
Asbert75 <erik_olsson_97@hotmai.com>
2016-03-17 13:08:45 +0100

    La till Index, JS, CSS

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+
+<title>EROS Content Management System</title>
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+<h1>EROS Content Management System</h1>
+<h2>Brought to you by E and O</h2>
+
+</body>
+</html>
